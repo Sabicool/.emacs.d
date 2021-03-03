796c99722e91d906e2349fab2bed1e21f6444bdc
Sabicool <saab.hoq@gmail.com>
2021-03-04 02:17:09 +0800

    Brought back org mode indenting

--- init.el
+++ init.el
@@ -329,8 +329,8 @@
   (gsetq  ;org-superstar-headline-bullets-list '("　")
 	  org-superstar-headline-bullets-list '("✱" "✦" "✸" "✿")
 	  org-hide-leading-stars t
-	  org-superstar-remove-leading-stars t
-	  org-hide t
+	 ;org-superstar-remove-leading-stars t
+	 ;org-hide t
 	 ; org-superstar-special-todo-items t
 	 ; org-superstar-todo-bullet-alist
 	 ; '(("TODO" "☐　")
@@ -347,7 +347,7 @@
  '(org-code ((t (:inherit (shadow fixed-pitch)))))
  '(org-document-info ((t (:foreground "dark orange"))))
  '(org-document-info-keyword ((t (:inherit (shadow fixed-pitch)))))
- '(org-indent ((t (:inherit (org-hide fixed-pitch)))))
+; '(org-indent ((t (:inherit (org-hide fixed-pitch)))))
  '(org-link ((t (:foreground "royal blue" :underline t))))
  '(org-meta-line ((t (:inherit (font-lock-comment-face fixed-pitch)))))
  '(org-property-value ((t (:inherit fixed-pitch))) t)
@@ -440,7 +440,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(TeX-source-correlate-mode t)
- '(blink-cursor-mode nil))
+ '(blink-cursor-mode nil)
+ '(org-agenda-files nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
